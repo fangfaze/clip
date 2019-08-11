c4053673ea1fd2df7c50c04cf1d21316026be177
chenze <ace50@163.com>
2019-08-11 20:55:05 +0800

    不完善的交换动画

--- src/components/tradediag.jsx
+++ src/components/tradediag.jsx
@@ -1,5 +1,10 @@
 import style from './tradediag.css'
-import { Row, Col, Button, Icon } from 'antd'
+import { Button, Icon } from 'antd'
+import React from 'react'
+
+import TweenOne from 'rc-tween-one';
+import BezierPlugin from 'rc-tween-one/lib/plugin/BezierPlugin';
+TweenOne.plugins.push(BezierPlugin);
 
 const Diaglog = () => {
     const text = '我对你的东西没有什么兴趣.'
@@ -12,20 +17,58 @@ const Diaglog = () => {
     )
 }
 
-const Trade = () => {
-    return (
-        <Row  type="flex" justify="space-around" align="middle">
-            <Col span={8}>
-                <div className={style.trade1}></div>
-            </Col>
-            <Col span={4} style = {{textAlign: 'center'}}>
-                <Button type = 'primary'><Icon type ='sync'></Icon>交换</Button>
-            </Col>
-            <Col span={8}>
-                <div className={style.trade2}></div>
-            </Col>
-        </Row>
-    )
+class Trade extends React.Component {
+
+    state = {
+        change: true,
+        moment: null
+    }
+    onClick = () => {
+        this.setState({ change: false, moment: null })
+    }
+    onFinish = (e) => {
+        console.log(e)
+        if ("onComplete" === e.mode) {
+            this.setState({ change: true, moment: 1 })
+        }
+    }
+
+    render() {
+        const anime1 = {
+            bezier: {
+                type: 'soft',
+                autoRotate: false,
+                vars: [
+                    { x: 0, y: 0 },
+                    { x: 150, y: 150 },
+                    { x: 300, y: 0 },
+                ],
+            }
+        }
+        const anime2 = {
+            bezier: {
+                type: 'soft',
+                autoRotate: false,
+                vars: [
+                    { x: 0, y: 0 },
+                    { x: -150, y: -150 },
+                    { x: -300, y: 0 },
+                ],
+            }
+        }
+        return (
+            <div className={style.tradebg}>
+                <TweenOne animation={anime1} paused={this.state.change} moment={this.state.moment} onChange={this.onFinish}>
+                    <div className={style.trade1}></div>
+                </TweenOne>
+                <Button type='primary' onClick={this.onClick}><Icon type='sync'></Icon>交换</Button>
+                <TweenOne animation={anime2} paused={this.state.change} moment={this.state.moment}>
+                    <div className={style.trade2}></div>
+                </TweenOne>
+            </div>
+        )
+    }
+
 }
 
 const Tradediag = () => {
@@ -35,7 +78,7 @@ const Tradediag = () => {
                 <div className={style.bg} ></div>
                 <Diaglog />
             </div>
-            <Trade  />
+            <Trade />
         </div>
     )
 }
