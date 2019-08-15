bc66b3ed5d5b44cbe09407622b127d29dbfae282
chenze <ace50@163.com>
2019-08-16 00:29:44 +0800

    对话

--- src/components/storydiag.jsx
+++ src/components/storydiag.jsx
@@ -1,15 +1,19 @@
 import style from './storydiag.css'
 
 
-const Diaglog = (saying, player) => {
+const Diaglog = ({ saying, player }) => {
     console.log(saying)
     console.log(player)
+
     return (
-        <div className={style.diag}>
-            <div className={style.pop1}></div>
-            <div className={style.pop2}></div>
-            <p>{saying.saying}</p>
-        </div>
+        <div className={style.top} style={player ? { direction: "rtl" } : {}}>
+            <div className={player ? style.bgdiagplayer : style.bg}></div>
+            <div className={style.diag} >
+                <div className={player ? style.pop1p : style.pop1}></div>
+                <div className={player ? style.pop2p : style.pop2}></div>
+                <p>{saying}</p>
+            </div>
+        </div >
     )
 }
 
@@ -17,14 +21,12 @@ const Diaglog = (saying, player) => {
 const StoryDiaglog = () => {
     return (
         <div>
-            <div className={style.top}>
-                <div className={style.bg} ></div>
-                <Diaglog saying = "你对力量一无所知!"/>
-                <Diaglog saying = "你对力量一无所知!" player/>
-                <Diaglog saying = "你对力量一无所知!"/>
-                <Diaglog saying = "你对力量一无所知!"/>
-            </div>
-        </div>
+            <Diaglog saying="你对力量一无所知!1" />
+            <Diaglog saying="你对力量才是一无所知!2" player />
+            <Diaglog saying="你对力量是真的一无所知!3" />
+            <Diaglog saying="你们全家都对力量是一无所知!4你们全家都对力量是一无所知你们全家都对力量是一无所知你们全家都对力量是一无所知" />
+            <Diaglog saying="你对力量才是一无所知你对力量才是一无所知你对力量才是一无所知你对力量才是一无所知你对力量才是一无所知你对力量才是一无所知你对力量才是一无所知!2可是" player />
+        </div >
     )
 }
 
